394c6abbd492634afb6c226cc3909847bfc01cc4
RobFireproof <rob_dodd6@hotmail.com>
2020-12-08 15:17:38 +0000

    Day 8

---
+++ src/console.rs
@@ -0,0 +1,78 @@
+use regex::Regex;
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Op {
+        Nop,
+        Acc,
+        Jmp
+    }
+#[derive(Clone)]
+pub struct Console {
+    prog: Vec<(Op, i32)>,
+    acc: i32,
+    pc: usize 
+}
+
+impl Console {
+    pub fn new(input: &str) -> Console {
+        let mut prog = Vec::new();
+        let inst_regex = Regex::new(r"^(\w+) ([+-]\d+)").unwrap();
+
+        for line in input.lines() {
+            let caps = inst_regex.captures(line.trim()).expect("Failed to parse input line {}");
+            let op = match &caps[1] {
+                "nop" => Op::Nop,
+                "acc" => Op::Acc,
+                "jmp" => Op::Jmp,
+                _ => unreachable!()
+            };
+
+            let param: i32 = caps[2].parse().expect("Failed to parse parameter");
+
+            prog.push((op, param));
+        }
+
+        Console {
+            prog,
+            acc: 0,
+            pc: 0
+        }
+    }
+
+    pub fn get_pc(&self) -> usize {
+        self.pc
+    }
+
+    pub fn get_acc(&self) -> i32 {
+        self.acc
+    }
+
+    pub fn step(&mut self) {
+        let (op, param) = &self.prog[self.pc];
+
+        match op {
+            Op::Nop => {
+                self.pc += 1;
+            }
+            Op::Jmp => {
+                self.pc = ((self.pc as i32) + *param) as usize; 
+            }
+            Op::Acc => {
+                self.acc += param;
+                self.pc += 1;
+            }
+        }
+    }
+
+    pub fn get_next_op(&self) -> Op {
+        self.prog[self.pc].0
+    }
+
+    pub fn set_next_op(&mut self, op: Op) {
+        self.prog[self.pc].0 = op;
+    }
+
+    pub fn terminated(&self) -> bool {
+        self.pc >= self.prog.len()        
+    }
+}
---
+++ src/day8.rs
@@ -0,0 +1,88 @@
+use std::{collections::HashSet, fs};
+use crate::console::Console;
+use crate::console::Op;
+use std::collections::VecDeque;
+
+#[aoc(day8, part1)]
+pub fn solve_part1(input: &str) -> i32 {
+    let mut cons = Console::new(input);
+    let mut used = HashSet::new();
+
+    while !used.contains(&cons.get_pc()) {
+        used.insert(cons.get_pc());
+        cons.step();
+    }
+
+    cons.get_acc()
+}
+
+#[aoc(day8, part2)]
+pub fn solve_part2(input: &str) -> i32 {
+    let orig = Console::new(input);
+    let mut queue = VecDeque::new();
+
+    queue.push_back(orig);
+    let mut is_first_run = true;
+
+    while queue.len() > 0 {
+        let mut cons = queue.pop_front().unwrap();
+        let mut used = HashSet::new();
+        println!("Trying new run...");
+
+        while !cons.terminated() && !used.contains(&cons.get_pc()) {
+            if is_first_run && 
+                cons.get_next_op() == Op::Jmp || 
+                cons.get_next_op() == Op::Nop {
+                    let mut copy = cons.clone();
+                    let new_op = if cons.get_next_op() == Op::Jmp {Op::Nop} else {Op::Jmp};
+                    copy.set_next_op(new_op);
+                    queue.push_back(copy);
+            }
+
+            used.insert(cons.get_pc());
+            cons.step();
+        }
+
+        is_first_run = false;
+
+        if cons.terminated() {
+            return cons.get_acc();
+        }
+    }
+
+    unreachable!()
+}
+
+#[test]
+fn day2_part1_test1() {
+    let test_data = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+    assert_eq!(solve_part1(test_data), 5);
+}
+
+#[test]
+fn day8_part2_test1() {
+    let test_data = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+    assert_eq!(solve_part2(test_data), 8);
+}
+
+#[test]
+fn day8_part2_test2() {
+    let test_data = fs::read_to_string(r"input/2020/day8.txt").expect("Something went wrong reading the file");
+    assert_eq!(solve_part2(&test_data), 8);
+}
