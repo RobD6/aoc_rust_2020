43a981ca2dc2521063701d3def2ddff465c99e58
RobD6 <robdodd6@googlemail.com>
2020-12-10 21:19:39 +0000

    Day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -30,8 +30,45 @@ pub fn solve_part1(input: &str) -> u32 {
 }
 
 #[aoc(day10, part2)]
-pub fn solve_part2(input: &str) -> u32 {
-0
+pub fn solve_part2(input: &str) -> u64 {
+    let mut num_paths = 1u64;
+
+    let mut nums: Vec<u32> = input.lines().map(|line| line.trim().parse().unwrap()).collect();
+    nums.sort();
+
+    let mut gap_run = 0;
+    let mut max_run = 0;
+
+    if nums[0] == 1 { gap_run = 1; }
+
+    for i in 0..nums.len() {
+        if i == nums.len()-1 ||
+           nums[i+1] - nums[i] == 3
+            {
+            //End of run of 1 gaps
+            max_run = std::cmp::max(gap_run, max_run);
+
+            let mut mul = 1;
+            match gap_run {
+                0 => {}
+                1 => {}
+                2 => {mul = 2;}
+                3 => {mul = 4;}
+                4 => {mul = 7;}
+                _ => unreachable!("Unknown gap size")
+            }
+
+            num_paths *= mul;
+
+            gap_run = 0;
+        }
+        else {
+            assert_eq!(nums[i+1] - nums[i], 1);
+            gap_run += 1;
+        }
+    }
+    println!("Biggest run was {}", max_run);
+    num_paths
 }
 
 #[test]
@@ -88,6 +125,52 @@ fn day10_part1_test2() {
 
 #[test]
 fn day10_part2_test1() {
-    let test_data = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc";
-    assert_eq!(solve_part2(test_data), 0);
+    let test_data = "16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+    assert_eq!(solve_part2(test_data), 8);
+}
+
+#[test]
+fn day10_part2_test2() {
+    let test_data = "28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+    assert_eq!(solve_part2(test_data), 19208);
 }
